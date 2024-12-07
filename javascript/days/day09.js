e021e7eaf66aba7fd4703bfd03d06e11d5706735
stellan.lausen <stellan-frederik.lausen@df-automotive.de>
2024-12-08 00:37:52 +0100

    add day09

---
+++ javascript/days/day09.js
@@ -0,0 +1,92 @@
+const squares  = document.querySelectorAll('.square');
+const mole     = document.querySelector('.mole');
+const timeLeft = document.querySelector('#time-left');
+const score    = document.querySelector('#score');
+const startBtn = document.querySelector('#start-button');
+const stopBtn  = document.querySelector('#stop-button');
+
+let isRunning = false
+let result = 0
+let moleTimer = null
+let moleId
+let gameTimer = 30
+let gameTimerId
+
+// Reset timer, score and mole
+const stopTimer = () => {
+    if (!isRunning) return;
+    isRunning = false;
+    clearInterval(moleTimer);
+    clearInterval(gameTimerId);
+    gameTimer = 30;
+    timeLeft.textContent = gameTimer;
+    result = 0
+    score.textContent = result
+};
+
+const randomSquare = () => {
+    squares.forEach(square => {
+        square.classList.remove('mole');
+        square.innerHTML = ''
+    })
+
+    const randomPosition = squares[Math.floor(Math.random() * squares.length)];
+    randomPosition.classList.add('mole')
+    randomPosition.innerHTML += '<img src="../../assets/images/memory/img-2.png" />';
+
+    let img = document.getElementsByTagName('img')[0]
+    setTimeout(() => {
+        img.style.marginTop = '20px'
+
+    }, 100);
+    setTimeout(() => {
+        img.style.marginTop = '200px'
+
+    }, 700);
+    moleId = randomPosition.id
+}
+
+squares.forEach(square => {
+    // When a mole is clicked remove the class and add point to score
+    square.addEventListener('mousedown', () => {
+        if (square.id === moleId) {
+            result++
+            score.textContent = result
+            square.classList.remove('mole');
+            square.innerHTML = ''
+        }
+    })
+})
+
+const moveTheMole = () => {
+    if (isRunning) return;
+    isRunning = true;
+    moleTimer = setInterval(() => randomSquare(), 1000);
+}
+
+// Start game or reload page if game ended
+startBtn.addEventListener('click', () => {
+    if (gameTimer == 0 && !isRunning) {
+        window.location.reload()
+    } else if (!isRunning) {
+        moveTheMole()
+        gameTimerId = setInterval(() => countdown(), 1000);
+    }
+});
+
+function countdown() {
+    if (gameTimer == 0 ) {
+        clearInterval(gameTimerId);
+        clearInterval(moleTimer);
+        alert('GAME OVER! Your score: ' + result)
+        squares.forEach(square => {
+            square.classList.remove('mole');
+        })
+        isRunning = false;
+    } else  {
+        gameTimer--;
+        timeLeft.textContent = gameTimer;
+    }
+}
+
+stopBtn.addEventListener('click', () => stopTimer())
